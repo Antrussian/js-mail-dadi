5a5d3f611b3c2c4e2c1955f1ff61343b41e57635
Antrussian <antonino.russo.agent@gmail.com>
2023-11-15 23:10:25 +0100

    Logica funzionante

---
+++ gioco-dei-dadi/js/script.js
@@ -0,0 +1,49 @@
+/*Gioco dei dadi
+Generare un numero random da 1 a 6, 
+Math. 
+
+sia per il giocatore sia per il computer.
+quindi const giocatore e const 
+
+Stabilire il vincitore, in base a chi fa il punteggio più alto. if if else else se player > di com  player < di com e se è = pareggio */
+
+
+
+const numeroDadoPlayer = Math.floor (Math.random() *6 +1);
+console.log(numeroDadoPlayer);
+
+
+const numeroDadoCom = Math.floor (Math.random() *6 +1);
+console.log(numeroDadoCom);
+
+
+let vincentePlayer = 'Il player ha vinto';
+
+let vincenteCom = 'Il com ha vinto';
+
+let pareggio = 'Pareggio';
+
+
+
+if (numeroDadoPlayer > numeroDadoCom) {
+
+  
+    
+    console.log( vincentePlayer);
+
+}
+
+else if (numeroDadoCom > numeroDadoPlayer) {
+
+    console.log( vincenteCom);
+   
+
+}
+
+else { (numeroDadoPlayer == numeroDadoCom) 
+
+    console.log(pareggio);
+
+
+}
+
